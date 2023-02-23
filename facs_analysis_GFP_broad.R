f5848eef29efe5df6ddf2488743f21ec91099cc3
joncchen <jonc.chen@gmail.com>
2023-02-23 14:31:11 -0500

    created a GFP version, and adjusted some gates according to data. feel free to move back as necessary.

--- facs_analysis_GFP_broad.R
+++ facs_analysis_GFP_broad.R
@@ -7,7 +7,7 @@ library('dplyr')
 library('broom')
 
 # FOR THE USER RUNNING THE CODE:set directory to get fcs files as set (CHANGE DIRECTORY ACCORDINGLY!)
-dir="/Users/joncchen/Dropbox (MIT)/Collins Lab: RNA-ligand screen/Raw Flow Files/2023-02-23/Exp_20230223_1/"
+dir="/Users/joncchen/Dropbox (MIT)/Collins Lab: RNA-ligand screen/Raw Flow Files/2023-02-21/Exp_20230221_1/GFP/"
 
 fs <- read.flowSet(path = dir,pattern = ".fcs",alter.names = T) #,truncate_max_range = FALSE)
 as.data.frame(pData(fs)$name)
@@ -15,11 +15,11 @@ as.data.frame(pData(fs)$name)
 #select positive and negative samples from table above
 #replace values below"
 #FOR THE USER RUNNING THE CODE put in the values depending on the generated table:
-pos_c=37
-neg_c=15
+pos_c=3
+neg_c=4
 
 ##change column names for ease of use
-colnames(fs)[colnames(fs)=="FL4.A"] <- "BFP"
+colnames(fs)[colnames(fs)=="FL9.A"] <- "GFP"
 colnames(fs)[colnames(fs)=="FL13.A"] <- "mCherry"
 
 #edit sample to name, keep well only
@@ -56,14 +56,14 @@ dev.off()
 
 ############# LIVE CHECKPOINT #############
 
-coor1 <- c(150e3, 140e1)
-coor2 <- c(120e3, 170e1)
-coor3 <- c(150e3, 220e1)
-coor4 <- c(210e3, 250e1)
-coor5 <- c(290e3, 2500)
-coor6 <- c(330e3, 2250)
-coor7 <- c(325e3, 1800)
-coor8 <- c(210e3, 1350)
+coor1 <- c(175e3, 140e1)
+coor2 <- c(145e3, 170e1)
+coor3 <- c(175e3, 220e1)
+coor4 <- c(235e3, 250e1)
+coor5 <- c(315e3, 2500)
+coor6 <- c(355e3, 2250)
+coor7 <- c(350e3, 1800)
+coor8 <- c(235e3, 1350)
 
 
 # define gate for singlets (ADJUST PARAMETERS ACCORDINGLY)
@@ -80,8 +80,8 @@ dev.off()
 
 coord1 <- c(19e4, 105e3)
 coord2 <- c(7e4, 170e3)
-coord3 <- c(46.5e4, 295e3)
-coord4 <- c(62.5e4, 260e3)
+coord3 <- c(53e4, 320e3)
+coord4 <- c(69e4, 290e3)
 
 # define gate for singlets2 (ADJUST PARAMETERS ACCORDINGLY)
 gs_pop_remove(gs, node="Singlets2")  ######## VARIABLE ##########
@@ -101,79 +101,79 @@ dev.off()
 #subset flowset to gated population
 fs_gated=gs_pop_get_data(gs,'Singlets2')
 
-# define constitutive reporter gate (BFP)
+# define constitutive reporter gate (GFP)
 #get gated data for postive and negative controls
 df_pos=as.data.frame(exprs(fs_gated[[pos_c]]))
 df_neg=as.data.frame(exprs(fs_gated[[neg_c]]))
 
 # coarsely fit a non-linear model for range evaluation
-model <- loess(mCherry ~ BFP, data = df_pos, span = 0.75)
+model <- loess(mCherry ~ GFP, data = df_pos, span = 0.75)
 df_pos <- augment(model, df_pos)
-model <- loess(mCherry ~ BFP, data = df_neg, span = 0.75)
+model <- loess(mCherry ~ GFP, data = df_neg, span = 0.75)
 df_neg <- augment(model, df_neg)
 
 #change values of xlim to evaluated the best reporter expression range
 # FOR THE USER RUNNING THE CODE, the numbers in front of xlim could be set depending on the x-axis range you want to see
-bfp_reporter_range <- ggplot(df_neg,aes(BFP,.fitted)) + geom_line(color='red') + geom_line(data=df_pos,aes(x=BFP,.fitted),color='blue') + xlim(250,80000)
+gfp_reporter_range <- ggplot(df_neg,aes(GFP,.fitted)) + geom_line(color='red') + geom_line(data=df_pos,aes(x=GFP,.fitted),color='blue') + xlim(250,80000)
 #save plot
 pdf(file=paste(dir,"model_test_to_set_reporter_range.pdf",sep=""),width = 10,height = 10)
-bfp_reporter_range
+gfp_reporter_range
 dev.off()
 
-# 
+
 # #set the reporter range for the gate
 # #you should manually put the range in Shiva!
 # # FOR THE USER RUNNING THE CODE you MUST put in the range that defines the bin for the data that will be analyzed! IMPORTANT!
 # #The (250, 80k) is for 300 ng per well of 48-well.
-# #gs_pop_remove(gs, node="BFP_pos")
-# g.bfp <- rectangleGate(filterId="BFP_pos",BFP=c(500,1e6))
+# gs_pop_remove(gs, node="GFP_pos")
+# g.gfp <- rectangleGate(filterId="GFP_pos",GFP=c(5000,1e6))
 # # check gate
-# gs_pop_add(gs,g.bfp,parent="Singlets2") # add gate to GatingSet
+# gs_pop_add(gs,g.gfp,parent="Singlets2") # add gate to GatingSet
 # recompute(gs) # recalculate Gatingset
-# BFP_hist <- ggcyto(gs,aes(x=BFP),subset="Singlets2",)+geom_density(fill="skyblue")+geom_gate("BFP_pos")+ geom_stats(adjust = 0.1,digits = 1)+scale_x_flowjo_biexp()+facet_wrap(~name,ncol = 8)
-# BFP_hist
+# GFP_hist <- ggcyto(gs,aes(x=GFP),subset="Singlets2",)+geom_density(fill="skyblue")+geom_gate("GFP_pos")+ geom_stats(adjust = 0.1,digits = 1)+scale_x_flowjo_biexp()+facet_wrap(~name,ncol = 8)
+# GFP_hist
 # #save plot
 # pdf(file=paste(dir,"reporter_gate.pdf",sep=""),width = 50,height = 50)
-# BFP_hist
+# GFP_hist
 # dev.off()
 
 
-coor1 <- c(685, 3.7e4)
-coor2 <- c(535, 15e4)
-coor3 <- c(635, 32e4)
-coor4 <- c(1000, 42e4)
+coor1 <- c(1185, 3.7e4)
+coor2 <- c(1035, 15e4)
+coor3 <- c(1135, 32e4)
+coor4 <- c(1500, 42e4)
 coor5 <- c(1e4, 52e4)
 coor6 <- c(1.8e5, 51e4)
 coor7 <- c(8e5, 35e4)
 coor8 <- c(5e5, 2.5e4)
 coor9 <- c(2.7e3, 1e4)
 
-gs_pop_remove(gs, node="BFP_pos")  ######## VARIABLE ##########
-g.bfp <- polygonGate(filterId = "BFP_pos","BFP"=c(coor1[1],coor2[1],coor3[1],coor4[1],coor5[1],coor6[1],coor7[1],coor8[1],coor9[1]),"SSC.A"=c(coor1[2],coor2[2],coor3[2],coor4[2],coor5[2],coor6[2],coor7[2],coor8[2],coor9[2]))
-gs_pop_add(gs,g.bfp,parent="Singlets2") # add gate to GatingSet
+gs_pop_remove(gs, node="GFP_pos")  ######## VARIABLE ##########
+g.gfp <- polygonGate(filterId = "GFP_pos","GFP"=c(coor1[1],coor2[1],coor3[1],coor4[1],coor5[1],coor6[1],coor7[1],coor8[1],coor9[1]),"SSC.A"=c(coor1[2],coor2[2],coor3[2],coor4[2],coor5[2],coor6[2],coor7[2],coor8[2],coor9[2]))
+gs_pop_add(gs,g.gfp,parent="Singlets2") # add gate to GatingSet
 recompute(gs) # recompute GatingSet
-bfp_dots <-ggcyto(gs,aes(x=BFP,y=SSC.A),subset="Singlets2")+geom_hex(bins = 200)+geom_gate(g.bfp)+ggcyto_par_set(limits = list(x=c(1,1e6),y=c(1e4,1e6))) + facet_wrap(~name,ncol = 8) + scale_x_log10() + geom_stats(adjust = 0.8)
+gfp_dots <-ggcyto(gs,aes(x=GFP,y=SSC.A),subset="Singlets2")+geom_hex(bins = 200)+geom_gate(g.gfp)+ggcyto_par_set(limits = list(x=c(1,1e6),y=c(1e4,1e6))) + facet_wrap(~name,ncol = 8) + scale_x_log10() + geom_stats(adjust = 0.8)
 
 #save plot
-pdf(file=paste(dir,"BFP_dots_gate.pdf",sep=""),width = 50,height = 50)
-bfp_dots
+pdf(file=paste(dir,"GFP_dots_gate.pdf",sep=""),width = 40,height = 10)
+gfp_dots
 dev.off()
 
 
 
 #subset flowset to gated population
-fs_gated=gs_pop_get_data(gs,'BFP_pos')
+fs_gated=gs_pop_get_data(gs,'GFP_pos')
 
 #repeat model fit for gate selection confirmation
 df_pos=as.data.frame(exprs(fs_gated[[pos_c]]))
 df_neg=as.data.frame(exprs(fs_gated[[neg_c]]))
 
 # coarsely fit a non-linear model for range evaluation
-model <- loess(mCherry ~ BFP, data = df_pos, span = 0.75)
+model <- loess(mCherry ~ GFP, data = df_pos, span = 0.75)
 df_pos <- augment(model, df_pos)
-model <- loess(mCherry ~ BFP, data = df_neg, span = 0.75)
+model <- loess(mCherry ~ GFP, data = df_neg, span = 0.75)
 df_neg <- augment(model, df_neg)
-final_model <- ggplot(df_neg,aes(BFP,.fitted)) + geom_line(color='red') + geom_line(data=df_pos,aes(x=BFP,.fitted),color='blue')
+final_model <- ggplot(df_neg,aes(GFP,.fitted)) + geom_line(color='red') + geom_line(data=df_pos,aes(x=GFP,.fitted),color='blue')
 final_model
 
 pdf(file=paste(dir,"updated_reporter_model.pdf",sep=""),width = 10,height = 10)
@@ -205,10 +205,10 @@ gating_rep
 write.csv(gating_rep,file=paste(dir,"gating_stats.csv",sep=""))
 
 #subset flowset to gated population
-fs_gated=gs_pop_get_data(gs,'BFP_pos')
+fs_gated=gs_pop_get_data(gs,'GFP_pos')
 
 mCherry_col <- which(colnames(fs_gated) == "mCherry")
-BFP_col <- which(colnames(fs_gated) == "BFP")
+GFP_col <- which(colnames(fs_gated) == "GFP")
 
 #get data for final gated population, remove negative mCHerry values. Median (not mean) is computed and plotted for all samples
 results=data.frame()
@@ -218,32 +218,32 @@ for (i in 1:length(rownames(pData(fs_gated)))){
   if (dim(df)[1] == 0){
     next
   } else {
-    df=df[,c(mCherry_col, BFP_col)]
+    df=df[,c(mCherry_col, GFP_col)]
     df$sample=rownames(pData(fs_gated[i]))
     results=rbind(results,df)
   }}
 results$sample=(results %>% tidyr::separate(sample,c('plate','bla','well',extra='drop')) %>% select('well'))[,1]
 
-p1=ggplot(results,aes(x=sample,y=mCherry/BFP)) + geom_boxplot() + theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
+p1=ggplot(results,aes(x=sample,y=mCherry/GFP)) + geom_boxplot() + theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
 p1
 #save the plot
 pdf(file=paste(dir,"single_sample_induction_boxplot.pdf",sep=""),width = 10,height = 10)
 p1
 dev.off()
 
-BFP_pos <- gating_rep[gating_rep$Population == '/Live/Singlets/Singlets2/BFP_pos']$Count
+GFP_pos <- gating_rep[gating_rep$Population == '/Live/Singlets/Singlets2/GFP_pos']$Count
 
 Total_counts <- gating_rep[gating_rep$Population == '/Live']$Count
 Singlets <- gating_rep[gating_rep$Population == '/Live/Singlets/Singlets2']$Count
 Live_percent <- 100*gating_rep[gating_rep$Population == '/Live']$percent_of_parent
-BFP_percent <- 100*gating_rep[gating_rep$Population == '/Live/Singlets/Singlets2/BFP_pos']$percent_of_parent
+GFP_percent <- 100*gating_rep[gating_rep$Population == '/Live/Singlets/Singlets2/GFP_pos']$percent_of_parent
 
-p2=ggplot(results,aes(x=sample,y=mCherry/BFP)) + geom_boxplot() + theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
+p2=ggplot(results,aes(x=sample,y=mCherry/GFP)) + geom_boxplot() + theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
 data_stats=as.data.frame(ggplot_build(p2)$data[[1]][,1:5])
 rownames(data_stats)=sort(unique(p1$data$sample))
-data_stats <- cbind(data_stats, Total_counts, Live_percent, Singlets, BFP_pos, BFP_percent)
+data_stats <- cbind(data_stats, Total_counts, Live_percent, Singlets, GFP_pos, GFP_percent)
 
 data_stats
 
 #write csv (open in excel) file with median stats
-write.csv(data_stats,file=paste(dir," median_stats.csv",sep=""))
+write.csv(data_stats,file=paste(dir,"hist_median_stats.csv",sep=""))
